3da5c92cde5a43b0329d2073dfda32cbb52dd5b3
wlinktl <wlinkcl@outlook.com>
2023-09-25 20:09:13 -0400

    initial commit

---
+++ src/test/java/com/demo/RecordsTest.java
@@ -0,0 +1,48 @@
+package com.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+class RecordsTest {
+
+    record User(String name, long accountNumber) {
+    }
+
+    record UserDeletedEvent(User user) {
+    }
+
+    record UserCreatedEvent(String name) {
+    }
+
+    record ShutdownEvent(Instant instant) {
+    }
+
+    @Test
+    void respondToEvents() throws Exception {
+        Assertions.assertEquals(
+                respond(new UserCreatedEvent("jlong")), "the new user with name jlong has been created"
+        );
+        Assertions.assertEquals(
+                respond(new UserDeletedEvent(new User("jlong", 1))),
+                "the user jlong has been deleted"
+        );
+    }
+
+    String respond(Object o) {
+        // ①
+        if (o instanceof ShutdownEvent(Instant instant)) {
+            System.out.println(
+                "going to to shutdown the system at " + instant.toEpochMilli());
+        }
+        return switch (o) {
+            // ②
+            case UserDeletedEvent(var user) -> "the user " + user.name() + " has been deleted";
+            // ③
+            case UserCreatedEvent(var name) -> "the new user with name " + name + " has been created";
+            default -> null;
+        };
+    }
+
+}
